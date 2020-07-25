9dce93e629c28f5deb92c950551c68cc4b21df8f
Lauren <TechnologicalType@MacBook-Air-2.local>
2020-07-25 11:38:31 -0400

    updated animations and modal - added Ryans birthday wishes also

--- js/extras.js
+++ js/extras.js
@@ -18,18 +18,25 @@ span.onclick = function() {
   modal.style.display = "none";
 };
 
-// Ryan's Birthday Wishes
 
-var modal = document.getElementById("myModal2");
+
+
+
+
+
+
+// Kristen's Birthday Wishes
+
+var modal2 = document.getElementById("myModal2");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg2");
-var modalImg = document.getElementById("img02");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+var img2 = document.getElementById("myImg2");
+var modalImg2 = document.getElementById("img02");
+var captionText2 = document.getElementById("caption");
+img2.onclick = function(){
+  modal2.style.display = "block";
+  modalImg2.src = this.src;
+  captionText2.innerHTML = this.alt;
 }
 
 // Get the <span> element that closes the modal
@@ -37,5 +44,36 @@ var span2 = document.getElementsByClassName("close2")[0];
 
 // When the user clicks on <span> (x), close the modal
 span2.onclick = function() {
-  modal.style.display = "none";
+  modal2.style.display = "none";
+};
+
+
+
+
+
+
+
+
+
+
+// Ryan's Birthday Wishes
+
+var modal3 = document.getElementById("myModal3");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img3 = document.getElementById("myImg3");
+var modalImg3 = document.getElementById("img03");
+var captionText3 = document.getElementById("caption");
+  img3.onclick = function(){
+  modal3.style.display = "block";
+  modalImg3.src = this.src;
+  captionText3.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span3 = document.getElementsByClassName("close3")[0];
+
+// When the user clicks on <span> (x), close the modal
+span3.onclick = function() {
+  modal3.style.display = "none";
 };
