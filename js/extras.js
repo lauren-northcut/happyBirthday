49bb183cd4cab0326b2707767d48360f0ea81640
Lauren <TechnologicalType@MacBook-Air-2.local>
2020-07-25 11:02:11 -0400

    added kristen's birthday wishes

--- js/extras.js
+++ js/extras.js
@@ -17,3 +17,25 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 };
+
+// Ryan's Birthday Wishes
+
+var modal = document.getElementById("myModal2");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg2");
+var modalImg = document.getElementById("img02");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal.style.display = "none";
+};
